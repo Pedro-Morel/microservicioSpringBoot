b10b022b9f635c2648f7ac188090014e12623b79
Pedro Morel <pedro.morel388@gmail.com>
2024-11-19 11:48:37 -0300

    Versionado y estructurado de URL

--- src/main/java/com/pedro/microservicioSpringBoot/controllers/CustomerController.java
+++ src/main/java/com/pedro/microservicioSpringBoot/controllers/CustomerController.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,7 +19,7 @@ public class CustomerController {
     private List<Customer> customers = new ArrayList<>(Arrays.asList(
             new Customer("123", "name1", "user1", "password1"),
             new Customer("456", "name2", "user2", "password2"),
-            new Customer("789", "name2", "user3", "password3")
+            new Customer("789", "name3", "user3", "password3")
     ));
 
     @GetMapping()
@@ -38,7 +40,15 @@ public class CustomerController {
     @PostMapping()
     public ResponseEntity<?> postCliente(@RequestBody Customer customer){
         customers.add(customer);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Cliente " + customer.getUsername() + " creado correctamente");
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().
+                path("/{username}").
+                buildAndExpand(customer.getUsername()).
+                toUri();
+
+       // return ResponseEntity.created(location).build();
+        return ResponseEntity.created(location).body(customer);
     }
 
     @PutMapping()
